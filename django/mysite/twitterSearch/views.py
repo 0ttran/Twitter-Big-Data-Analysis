703e72f28b19e30a913b395f06f67f657e730594
Tien Tran <ttran067@yahoo.com>
2016-05-26 14:09:46 -0700

    added error checking for invalid inputs

--- django/mysite/twitterSearch/views.py
+++ django/mysite/twitterSearch/views.py
@@ -4,6 +4,8 @@ from django.template.loader import get_template
 from django.shortcuts import render
 from django.http import HttpResponse
 from cassandra.cluster import Cluster
+
+from geopy import geocoders
 from geopy.geocoders import Nominatim
 
 # Get information from database
@@ -11,31 +13,37 @@ def getInfoFromDB(stringval):
 	geolocator = Nominatim()
 	try:
 		location = geolocator.geocode(str(stringval))
-	except (ValueError, GQueryError):
-		return
-	longitude = int(location.longitude)
-	latitude = int(location.latitude)
+		longitude = int(location.longitude)
+		latitude = int(location.latitude)
 
-	#Retrive the top 3 most popular hashtag from database
-	node = ['127.0.0.1']
-	cluster = Cluster(node)
-	session = cluster.connect()
-	session.set_keyspace("tweets")
-	out = session.execute('select max(occurence) from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' ')[0]
-	mostPopularHashtag = session.execute('select hashtag from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence=' + str(out.system_max_occurence) + ' ')[0]
-	out2 = session.execute('select max(occurence) from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence<' + str(out.system_max_occurence) + " ")[0]
-	popularhashtags2 = session.execute('select hashtag from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence=' + str(out2.system_max_occurence) + ' ')[0]
-	out3 = session.execute('select max(occurence) from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence<' + str(out2.system_max_occurence) + " ")[0]
-	popularhashtags3 = session.execute('select hashtag from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence=' + str(out3.system_max_occurence) + ' ')[0]
-	
-	returnVal = {
-	'mostPopularHash': mostPopularHashtag.hashtag,
-	'mostPopularHash2': popularhashtags2.hashtag,
-	'mostPopularHash3': popularhashtags3.hashtag,
-	'origString': stringval,
-	}
+		#Retrive the top 3 most popular hashtag from database
+		node = ['127.0.0.1']
+		cluster = Cluster(node)
+		session = cluster.connect()
+		session.set_keyspace("tweets")
+		out = session.execute('select max(occurence) from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' ')[0]
+		mostPopularHashtag = session.execute('select hashtag from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence=' + str(out.system_max_occurence) + ' ')[0]
+		out2 = session.execute('select max(occurence) from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence<' + str(out.system_max_occurence) + " ")[0]
+		popularhashtags2 = session.execute('select hashtag from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence=' + str(out2.system_max_occurence) + ' ')[0]
+		out3 = session.execute('select max(occurence) from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence<' + str(out2.system_max_occurence) + " ")[0]
+		popularhashtags3 = session.execute('select hashtag from tweets.popularhashtags where longitude=' + str(longitude) + ' and latitude=' + str(latitude) + ' and occurence=' + str(out3.system_max_occurence) + ' ')[0]
+		
+		returnVal = {
+		'mostPopularHash': mostPopularHashtag.hashtag,
+		'mostPopularHash2': popularhashtags2.hashtag,
+		'mostPopularHash3': popularhashtags3.hashtag,
+		'origString': stringval,
+		}
 
-	return returnVal
+		return returnVal
+	except:
+		returnVal = {
+		'mostPopularHash': 'No results found, please try again.',
+		'mostPopularHash2': '',
+		'mostPopularHash3': '',
+		'origString': 'usa',
+		}
+		return returnVal
 
 # index handles server sided requests
 def index(request):
